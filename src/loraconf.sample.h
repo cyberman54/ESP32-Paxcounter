ce3f7c09f6b562cbe7b61f53d5fcfd99473cedea
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-03-25 23:43:26 +0200

    comments in loraconf_sample.h changed

--- src/loraconf.sample.h
+++ src/loraconf.sample.h
@@ -18,10 +18,7 @@ static const u1_t DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 // For TTN, APPEUI always starts with 0x70, 0xB3, 0xD5
 static const u1_t APPEUI[8]={ 0x70, 0xB3, 0xD5, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-// This key should be in big endian format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-// The key shown here is the semtech default key.
+// Note: Use msb format for APPEUI as in TTN console (cut & paste, for your convenience)
 static const u1_t APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 */
