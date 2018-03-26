466e49d998c6d64f016a24e050a6622fb41c1a3e
Charles <hallard04@free.fr>
2018-03-27 01:51:48 +0200

    Creation

---
+++ src/hal/lolin32_lora.h
@@ -0,0 +1,24 @@
+// Hardware related definitions for lolin32_lora shield
+// See https://github.com/hallard/LoLin32-Lora
+
+#define HAS_DISPLAY U8X8_SSD1306_128X64_NONAME_HW_I2C // OLED-Display on board
+#define HAS_LED GPIO_NUM_25 // white LED on board
+#define HAS_BUTTON 15 // button "PROG" on board
+#define CFG_sx1276_radio 1
+
+// re-define pin definitions of pins_arduino.h
+#define PIN_SPI_SS    5  // ESP32 GPIO5   (Pin18) -- SX1276 NSS (Pin19) SPI Chip Select Input
+#define PIN_SPI_MOSI  23 // ESP32 GPIO27 (Pin27) -- SX1276 MOSI (Pin18) SPI Data Input
+#define PIN_SPI_MISO  19 // ESP32 GPIO19 (Pin19) -- SX1276 MISO (Pin17) SPI Data Output
+#define PIN_SPI_SCK   18 // ESP32 GPIO5 (Pin5)   -- SX1276 SCK (Pin16) SPI Clock Input
+
+// non arduino pin definitions
+#define RST   25 // ESP32 GPIO14 (Pin14) -- SX1276 NRESET (Pin7) Reset Trigger Input
+#define DIO0  27 // ESP32 GPIO26 (Pin15) -- SX1276 DIO0 (Pin8) used by LMIC for detecting LoRa RX_Done & TX_Done
+#define DIO1  26 // ESP32 GPIO33 (Pin13) -- SX1276 DIO1 (Pin9) used by LMIC for detecting LoRa RX_Timeout
+#define DIO2   4 // ESP32 GPIO32 (Pin12) -- SX1276 DIO2 (Pin10) not used by LMIC for LoRa (Timeout for FSK only)
+
+// Hardware pin definitions for Heltec LoRa-32 Board with OLED SSD1306 I2C Display
+#define OLED_RST NOT_A_PIN
+#define OLED_SDA 21  // ESP32 GPIO4 (Pin4)   -- SD1306 D1+D2
+#define OLED_SCL 22 // ESP32 GPIO15 (Pin15) -- SD1306 D0
