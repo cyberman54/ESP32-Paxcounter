b854f09204bb9affbadd3b0707cd41241d9efa93
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-04-02 22:45:08 +0200

    v1.2.85

--- src/main.h
+++ src/main.h
@@ -1,5 +1,5 @@
 // program version
-#define PROGVERSION                     "1.2.83"    // use max 10 chars here!
+#define PROGVERSION                     "1.2.85"    // use max 10 chars here!
 #define PROGNAME                        "PAXCNT"
 
 // Verbose enables serial output
