9315810028acd5cab761b26ad0e56323d7218981
Charles <hallard04@free.fr>
2018-04-06 10:56:21 +0200

    Removed sample config with defines

--- src/loraconf.sample.h
+++ src/loraconf.sample.h
@@ -14,15 +14,7 @@
 // Note: Use same format as in TTN console (cut & paste, for your convenience)
 // *** Take care : If Using a board with Microchip 24AA02E64 Uinique ID for deveui, **
 // *** this DEVEUI will be overwriten by the one contained in the Microchip module ***
-#if defined (ttgov2)
-static const u1_t DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02 };
-#elif defined (ttgov1)
-static const u1_t DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
-#elif defined (heltec_wifi_lora_32)
-static const u1_t DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x32 };
-#else
 static const u1_t DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-#endif
 
 // Note: Use msb format for APPEUI as in TTN console (cut & paste, for your convenience)
 // For TTN, APPEUI always starts with 0x70, 0xB3, 0xD5
