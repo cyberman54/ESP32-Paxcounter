54945f173ecc5fa68f63a235ae393e5896eb43fa
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-04-19 11:09:17 +0200

    main.cpp: code sanitization

--- src/main.cpp
+++ src/main.cpp
@@ -74,16 +74,14 @@ int redirect_log(const char * fmt, va_list args) {
 
 //--- Prototypes ---
 
-// LMIC functions, defined in this main.cpp
-void onEvent(ev_t ev);
-void do_send(osjob_t* j);
-
 // defined in configmanager.cpp
 void eraseConfig(void);
 void saveConfig(void);
 void loadConfig(void);
 
 // defined in lorawan.cpp
+void onEvent(ev_t ev);
+void do_send(osjob_t* j);
 void gen_lora_deveui(uint8_t * pdeveui);
 void RevBytes(unsigned char* b, size_t c);
 void get_hard_deveui(uint8_t *pdeveui);
