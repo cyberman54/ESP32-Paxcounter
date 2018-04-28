221b06d3c67e25cee03b742689441ab0e7552518
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-04-28 14:33:27 +0200

    v1.3.4

--- src/main.h
+++ src/main.h
@@ -1,6 +1,6 @@
 
 // program version - note: increment version after modifications to configData_t struct!!
-#define PROGVERSION                     "1.3.35"    // use max 10 chars here!
+#define PROGVERSION                     "1.3.4"    // use max 10 chars here!
 #define PROGNAME                        "PAXCNT"
 
 //--- Declarations ---
