06e68459c85b646d8516f38fb0a9f0b9314342d8
Cyberman54 <cyberman@obh.snafu.de>
2018-05-11 12:40:36 +0200

    loraconf.sample.h modified

--- src/loraconf.sample.h
+++ src/loraconf.sample.h
@@ -7,8 +7,6 @@
 
 #include <Arduino.h>
 
-/* 
-
 // Set your DEVEUI here, if you have one. You can leave this untouched, 
 // then the DEVEUI will be generated during runtime from device's MAC adress
 // Note: Use same format as in TTN console (cut & paste, for your convenience)
@@ -22,5 +20,3 @@ static const u1_t APPEUI[8]={ 0x70, 0xB3, 0xD5, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 // Note: Use msb format for APPEUI as in TTN console (cut & paste, for your convenience)
 static const u1_t APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
-*/
