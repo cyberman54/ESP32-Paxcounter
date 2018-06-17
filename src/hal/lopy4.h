b5f735a6d3842a8e543046e99925d9ee2d90f04d
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-06-17 14:01:55 +0200

    lopy.h + lopy4.h corrected Pytrack settings

--- src/hal/lopy4.h
+++ src/hal/lopy4.h
@@ -4,14 +4,15 @@
 #define HAS_LED NOT_A_PIN // LoPy4 has no on board LED, so we use RGB LED on LoPy4
 #define HAS_RGB_LED   GPIO_NUM_0  // WS2812B RGB LED on GPIO0
 
-// !!EXPERIMENTAL - not tested yet!!f
-// uncomment this only if your LoPy lives on a Pytrack expansion board with GPS
+// !!EXPERIMENTAL - not tested yet!!
+// uncomment this only if your LoPy runs on a Pytrack expansion board with GPS
 // see http://www.quectel.com/UploadImage/Downlad/Quectel_L76-L_I2C_Application_Note_V1.0.pdf
 //#define HAS_GPS 1
-//#define GPS_I2C_PINS GPIO_NUM_9, GPIO_NUM_8 // SDA, SCL
+//#define GPS_I2C_PINS GPIO_NUM_25, GPIO_NUM_26 // SDA (P22), SCL (P21)
 //#define GPS_I2C_ADDRESS_READ 0x21
 //#define GPS_I2C_ADDRESS_WRITE 0x20
-//#define HAS_BUTTON GPIO_NUM_4
+//#define HAS_BUTTON GPIO_NUM_37 // (P14)
+//#define BUTTON_PULLUP 1  // Button need pullup instead of default pulldown
 
 // Hardware pin definitions for Pycom LoPy4 board
 #define PIN_SPI_SS    GPIO_NUM_18
