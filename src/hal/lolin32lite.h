9ddef4eb6451c9b29b1388fdab72e5ab9fe269d6
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-07-15 22:28:12 +0200

    lolin32lite.h edited

--- src/hal/lolin32lite.h
+++ src/hal/lolin32lite.h
@@ -2,14 +2,7 @@
 
 #define CFG_sx1272_radio 1 // dummy
 
-// disable brownout detection (avoid unexpected reset on some boards)
-#define DISABLE_BROWNOUT 1 // comment out if you want to keep brownout feature
-
 #define HAS_LED 22        // on board LED on GPIO22
 #define LED_ACTIVE_LOW 1  // Onboard LED is active when pin is LOW
 
 #define HAS_SPI   1       // comment out if device shall not send data via SPI
-
-// I2C config for Microchip 24AA02E64 DEVEUI unique address
-#define MCP_24AA02E64_I2C_ADDRESS 0x50 // I2C address for the 24AA02E64 
-#define MCP_24AA02E64_MAC_ADDRESS 0xF8 // Memory adress of unique deveui 64 bits
