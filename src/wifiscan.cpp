6e383e16291d53ceccd37792af895d36a2f50e2b
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-11-25 11:52:47 +0100

    Issue #206

--- src/wifiscan.cpp
+++ src/wifiscan.cpp
@@ -9,6 +9,21 @@ static wifi_country_t wifi_country = {WIFI_MY_COUNTRY, WIFI_CHANNEL_MIN,
                                       WIFI_CHANNEL_MAX, 100,
                                       WIFI_COUNTRY_POLICY_MANUAL};
 
+typedef struct {
+  unsigned frame_ctrl : 16;
+  unsigned duration_id : 16;
+  uint8_t addr1[6]; // receiver address
+  uint8_t addr2[6]; // sender address
+  uint8_t addr3[6]; // filtering address
+  unsigned sequence_ctrl : 16;
+  uint8_t addr4[6]; // optional
+} wifi_ieee80211_mac_hdr_t;
+
+typedef struct {
+  wifi_ieee80211_mac_hdr_t hdr;
+  uint8_t payload[0]; // network data ended with 4 bytes csum (CRC32)
+} wifi_ieee80211_packet_t;
+
 // using IRAM_:ATTR here to speed up callback function
 IRAM_ATTR void wifi_sniffer_packet_handler(void *buff,
                                            wifi_promiscuous_pkt_type_t type) {
