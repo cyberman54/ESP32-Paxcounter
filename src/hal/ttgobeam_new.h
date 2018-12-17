82c831e4e4a7d2410e25f1318efbeb51e636307b
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2018-12-17 10:42:34 +0100

    update ttgobeam_new.h

--- src/hal/ttgobeam_new.h
+++ src/hal/ttgobeam_new.h
@@ -6,13 +6,14 @@
 #include <stdint.h>
 
 // Hardware related definitions for TTGO T-Beam board
-
+//
+// pinouts taken from http://tinymicros.com/wiki/TTGO_T-Beam
+//
 // enable only if device has these sensors, otherwise comment these lines
 // BME680 sensor on I2C bus
-// attention: Pin21 is also LED! set HAS_LED to NOT_A_PIN if using BME280
 // don't forget to connect SDIO of BME680 to GND for selecting i2c addr 0x76
+//
 //#define HAS_BME GPIO_NUM_21, GPIO_NUM_22 // SDA, SCL
-//#define HAS_LED NOT_A_PIN // on board green LED
 
 #define HAS_LED GPIO_NUM_14 // on board green LED
 
@@ -25,7 +26,7 @@
 
 //#define DISABLE_BROWNOUT 1 // comment out if you want to keep brownout feature
 
-#define HAS_BUTTON GPIO_NUM_39 // on board button "BOOT" (next to reset button)
+#define HAS_BUTTON GPIO_NUM_39 // on board button "SW5"
 
 #define HAS_BATTERY_PROBE ADC1_GPIO35_CHANNEL // battery probe GPIO pin -> ADC1_CHANNEL_7
 #define BATT_FACTOR 2 // voltage divider 100k/100k on board
@@ -37,7 +38,7 @@
 #define LORA_CS   (18)
 #define LORA_MISO (19)
 #define LORA_MOSI (27)
-#define LORA_RST   LMIC_UNUSED_PIN
+#define LORA_RST  (23)
 #define LORA_IRQ  (26)
 #define LORA_IO1  (33)  // for T-Beam T22_V05 and T22_V07, for other versions use ttgobeam_old.h
 #define LORA_IO2  (32)
