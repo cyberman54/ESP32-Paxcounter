0f614da07bc682ea298488ea0f0c319cabae2270
Klaus K Wilting <Klaus.K.Wilting@deutschebahn.com>
2019-01-21 17:49:58 +0100

    ttgofox.h: update LORA_RST

--- src/hal/ttgofox.h
+++ src/hal/ttgofox.h
@@ -9,7 +9,7 @@
 #define CFG_sx1276_radio 1 // HPD13A LoRa SoC
 
 #define HAS_DISPLAY U8X8_SSD1306_128X64_NONAME_HW_I2C
-#define HAS_LED NOT_A_PIN // green on board LED is useless, because it's wired to GPIO25 = LORA_RST
+#define HAS_LED NOT_A_PIN // green on board LED is useless, is GPIO25, but also switching Power for Lora+Display
 //#define LED_ACTIVE_LOW 1  // Onboard LED is active when pin is LOW
 #define HAS_BATTERY_PROBE ADC1_GPIO35_CHANNEL
 #define BATT_FACTOR 2 // voltage divider 100k/100k on board
@@ -28,7 +28,7 @@
 #define LORA_CS   (18)
 #define LORA_MISO (19)
 #define LORA_MOSI (27)
-#define LORA_RST  (25)
+#define LORA_RST  (23)
 #define LORA_IRQ  (26)
 #define LORA_IO1  (33)
 #define LORA_IO2  (32)
