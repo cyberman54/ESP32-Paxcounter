8de2ee0680aeb88fefb49817e918c9f41e05c6c6
Verkehrsrot <cyberman@obh.snafu.de>
2019-03-23 15:06:05 +0100

    edit  board file ecopower.h

--- src/hal/ecopower.h
+++ src/hal/ecopower.h
@@ -6,10 +6,8 @@
 #include <stdint.h>
 
 #define HAS_LORA 1 // comment out if device shall not send data via LoRa or has no LoRa
-
 #define CFG_sx1276_radio 1 // select LoRa chip
 
-//#define BOARD_HAS_PSRAM // use if board has external PSRAM
 //#define DISABLE_BROWNOUT 1 // comment out if you want to keep brownout feature
 
 #define HAS_DISPLAY U8X8_SSD1306_128X64_NONAME_HW_I2C
@@ -23,7 +21,7 @@
 #define EXT_POWER_OFF   1
 
 #define HAS_LED (12) // on board  LED
-//#define HAS_BUTTON (0) // on board button
+//#define HAS_BUTTON (0) // on board button -> don't use, is same as RTC_INT!
 
 // Pins for I2C interface of OLED Display
 #define MY_OLED_SDA (21)
@@ -31,12 +29,13 @@
 #define MY_OLED_RST U8X8_PIN_NONE
 
 // Settings for on board DS3231 RTC chip
-// note: to use RTC_INT, ext_power_on is needed!
+// note: to use RTC_INT, capacitor 100nF next to red LED must be removed to sharpen interrupt signal slope
+// and setting EXT_POWER_ON is needed (this is done in main.cpp)
 #define HAS_RTC MY_OLED_SDA, MY_OLED_SCL // SDA, SCL
-#define RTC_INT GPIO_NUM_0
+#define RTC_INT GPIO_NUM_0 //
 
 // Settings for IF482 interface
-#define HAS_IF482 9600, SERIAL_7E1, GPIO_NUM_27, GPIO_NUM_14 // JP8 #12/#13
+#define HAS_IF482 9600, SERIAL_7E1, GPIO_NUM_3, GPIO_NUM_1 // RX, TX
 
 // Settings for DCF77 interface
 //#define HAS_DCF77 GPIO_NUM_14 // JP8 #13
@@ -49,7 +48,7 @@
 #define LORA_MOSI (23)
 #define LORA_RST  (17)
 #define LORA_IRQ  (16)
-#define LORA_IO1  (4) // JP9 #8 to be external wired
+#define LORA_IO1  (14) // JP8 #13 to be external wired
 #define LORA_IO2  LMIC_UNUSED_PIN
 
 #endif
