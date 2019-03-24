c0abe0652e4fb20f189fcb94658a95e5bbae836f
Verkehrsrot <cyberman@obh.snafu.de>
2019-03-24 16:16:58 +0100

    timesync params moved to paxcounter.conf

--- include/timesync.h
+++ include/timesync.h
@@ -6,12 +6,9 @@
 #include "timesync.h"
 #include "timekeeper.h"
 
-#define TIME_SYNC_SAMPLES 1   // number of time requests for averaging
-#define TIME_SYNC_CYCLE 20    // delay between two time samples [seconds]
-#define TIME_SYNC_TIMEOUT 120 // timeout waiting for timeserver answer [seconds]
-#define TIME_SYNC_TRIGGER 100 // deviation triggering a time sync [milliseconds]
+//#define TIME_SYNC_TRIGGER 100 // threshold for time sync [milliseconds]
 #define TIME_SYNC_FRAME_LENGTH 0x06 // timeserver answer frame length [bytes]
-#define TIME_SYNC_FIXUP 30 // calibration to fixup processing time [milliseconds]
+#define TIME_SYNC_FIXUP 6 // calibration to fixup processing time [milliseconds]
 
 void send_timesync_req(void);
 int recv_timesync_ans(uint8_t buf[], uint8_t buf_len);
