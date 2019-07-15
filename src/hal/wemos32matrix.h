72e36eb5542600101625fa095c692f46063ecb99
Verkehrsrot <cyberman@obh.snafu.de>
2019-07-15 22:40:50 +0200

    update wemos32matrix.h

--- src/hal/wemos32matrix.h
+++ src/hal/wemos32matrix.h
@@ -15,15 +15,21 @@
 #define LED_MATRIX_HEIGHT               16      // Height in pixels (LEDs ) of your display
 
 // Pin numbers work fine for Wemos Lolin32 board (all used pins are on 1 side of the board)
+// Explanation of pin signals see https://learn.adafruit.com/32x16-32x32-rgb-led-matrix/new-wiring
 #define MATRIX_DISPLAY_SCAN_US          500     // Matrix display scan rate in microseconds (1ms is about 'acceptable')
-#define LED_MATRIX_LATCHPIN             13      // Connects to LAT pin on display (Latch)
-#define LED_MATRIX_CLOCKPIN             32      // Connects to CLK pin on display (Clock)
-#define LED_MATRIX_EN_74138             12      // Connects to EN pin on display (Output Enable)
-#define LED_MATRIX_LA_74138             14      // Connects to LA pin on display
-#define LED_MATRIX_LB_74138             27      // Connects to LB pin on display
-#define LED_MATRIX_LC_74138             25      // Connects to LC pin on display
-#define LED_MATRIX_LD_74138             26      // Connects to LD pin on display
-#define LED_MATRIX_DATA_R1              33      // Connects to R1 pin on display
+#define LED_MATRIX_LATCHPIN             13      // LAT (or STB = Strobe)
+#define LED_MATRIX_CLOCKPIN             32      // CLK
+#define LED_MATRIX_EN_74138             12      // EN (or OE)
+#define LED_MATRIX_LA_74138             14      // LA (or A)
+#define LED_MATRIX_LB_74138             27      // LB (or B)
+#define LED_MATRIX_LC_74138             25      // LC (or C)
+#define LED_MATRIX_LD_74138             26      // LD (or D)
+#define LED_MATRIX_DATA_R1              33      // R1 (or R0)
+
+// CLK: The clock signal moves data bits from pin R1 ("red") in the shift registers
+// LAT: The latch signal enables LEDs according to shift register contents
+// Line Selects: LA, LB, LC, LD select which two rows of the display are currently lit
+// OE: Output enable switches the LEDs on/off while transitioning from one row to the next
 
 #define DISABLE_BROWNOUT 1 // comment out if you want to keep brownout feature
 
