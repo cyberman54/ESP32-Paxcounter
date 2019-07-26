dc540e15b33a3413ca969c9701b8c1cee406e1f8
Verkehrsrot <cyberman@obh.snafu.de>
2019-07-26 12:11:23 +0200

    BOSCH BSEC update to v1.4.7.4

--- include/bmesensor.h
+++ include/bmesensor.h
@@ -15,15 +15,15 @@ extern bmeStatus_t
     bme_status; // Make struct for storing gps data globally available
 extern TaskHandle_t BmeTask;
 
-// --- Bosch BSEC v1.4.7.3 library configuration ---
+// --- Bosch BSEC v1.4.7.4 library configuration ---
 // 3,3V supply voltage; 3s max time between sensor_control calls; 4 days
 // calibration. Change this const if not applicable for your application (see
 // BME680 datasheet)
 const uint8_t bsec_config_iaq[454] = {
-    3,   7,   4,   1,   61,  0,   0,   0,   0,   0,   0,   0,   174, 1,   0,
+    4,   7,   4,   1,   61,  0,   0,   0,   0,   0,   0,   0,   174, 1,   0,
     0,   48,  0,   1,   0,   0,   192, 168, 71,  64,  49,  119, 76,  0,   0,
     225, 68,  137, 65,  0,   63,  205, 204, 204, 62,  0,   0,   64,  63,  205,
-    204, 204, 62,  0,   0,   0,   0,   0,   80,  5,   95,  0,   0,   0,   0,
+    204, 204, 62,  0,   0,   0,   0,   216, 85,  0,   100, 0,   0,   0,   0,
     0,   0,   0,   0,   28,  0,   2,   0,   0,   244, 1,   225, 0,   25,  0,
     0,   128, 64,  0,   0,   32,  65,  144, 1,   0,   0,   112, 65,  0,   0,
     0,   63,  16,  0,   3,   0,   10,  215, 163, 60,  10,  215, 35,  59,  10,
@@ -50,7 +50,7 @@ const uint8_t bsec_config_iaq[454] = {
     255, 255, 255, 220, 5,   220, 5,   255, 255, 255, 255, 255, 255, 255, 255,
     255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
     255, 255, 255, 255, 255, 255, 255, 255, 255, 44,  1,   0,   0,   0,   0,
-    253, 65,  0,   0};
+    138, 80,  0,   0};
 
 // Helper functions declarations
 int bme_init();
