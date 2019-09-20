f8a115ac17d2e5670f79f4d0d893205bff96a09a
Verkehrsrot <cyberman@obh.snafu.de>
2019-09-20 19:23:43 +0200

    revert ADR display inverse logic

--- src/display.cpp
+++ src/display.cpp
@@ -203,10 +203,10 @@ void draw_page(time_t t, uint8_t page) {
 
 #if (HAS_LORA)
     u8x8.setCursor(11, 3);
-    if (cfg.adrmode) // if ADR=on then display SF value inverse
+    if (!cfg.adrmode) // if ADR=off then display SF value inverse
       u8x8.setInverseFont(1);
     u8x8.printf("%5s", getSfName(updr2rps(LMIC.datarate)));
-    if (cfg.adrmode) // switch off inverse if it was turned on
+    if (!cfg.adrmode) // switch off inverse if it was turned on
       u8x8.setInverseFont(0);
 #endif // HAS_LORA
 
