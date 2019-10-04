ba732012b9d251c0e2591941a628f73bce8e4636
Marius Gripp <m.gripp@greyrook.com>
2019-10-04 15:31:31 +0200

    move timesync seq_no into message body from port

--- include/timesync.h
+++ include/timesync.h
@@ -12,7 +12,9 @@
 
 void timesync_init(void);
 void send_timesync_req(void);
-int recv_timesync_ans(const uint8_t seq_no, const uint8_t buf[], const uint8_t buf_len);
+
+int recv_timesync_ans(const uint8_t buf[], uint8_t buf_len);
+
 void process_timesync_req(void *taskparameter);
 void store_time_sync_req(uint32_t t_millisec);
 
