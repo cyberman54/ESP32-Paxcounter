97b02d34b93acedfdeebeaedb00c6b2e71b4c9fd
Verkehrsrot <cyberman@obh.snafu.de>
2019-12-21 14:27:33 +0100

    i2c functions moved from power.cpp to i2c.cpp

--- include/power.h
+++ include/power.h
@@ -22,8 +22,6 @@ void AXP192_powerevent_IRQ(void);
 void AXP192_power(pmu_power_t powerlevel);
 void AXP192_init(void);
 void AXP192_showstatus(void);
-uint8_t i2c_writeBytes(uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len);
-uint8_t i2c_readBytes(uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len);
 
 #endif // HAS_PMU
 
