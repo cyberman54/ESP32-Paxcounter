9abd0e7a0716a611ee817d6cb9a4c74c9e065e34
Klaus K Wilting <klaus.k.wilting@deutschebahn.com>
2020-03-15 18:09:41 +0100

    switch AES to avoid compiler warnings in lmic.c

--- src/lmic_config.h
+++ src/lmic_config.h
@@ -87,12 +87,12 @@
 // implementation is optimized for speed on 32-bit processors using
 // fairly big lookup tables, but it takes up big amounts of flash on the
 // AVR architecture.
-#define USE_ORIGINAL_AES
+//#define USE_ORIGINAL_AES
 //
 // This selects the AES implementation written by Ideetroon for their
 // own LoRaWAN library. It also uses lookup tables, but smaller
 // byte-oriented ones, making it use a lot less flash space (but it is
 // also about twice as slow as the original).
-// #define USE_IDEETRON_AES
+#define USE_IDEETRON_AES
 //
 //#define USE_MBEDTLS_AES
