c6a41ff66668fe3ad6ed528a8441b18095517752
August Quint <49277349+AugustQu@users.noreply.github.com>
2020-09-24 14:31:51 +0200

    Update ttgov21new.h
    adding the pins for the SD-card on this board

--- src/hal/ttgov21new.h
+++ src/hal/ttgov21new.h
@@ -17,6 +17,11 @@
 
 // enable only if you want to store a local paxcount table on the device
 #define HAS_SDCARD  1      // this board has an SD-card-reader/writer
+// Pins for SD-card
+#define SDCARD_CS    (13)
+#define SDCARD_MOSI  (15)
+#define SDCARD_MISO  (2)
+#define SDCARD_SCLK  (14)
 
 #define HAS_DISPLAY 1
 #define HAS_LED (25) // green on board LED
