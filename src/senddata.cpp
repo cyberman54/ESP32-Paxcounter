e3858eceef7605e5e8bd5346109f8c269e23affd
Klaus K Wilting <klaus.k.wilting@deutschebahn.com>
2020-10-09 19:56:29 +0200

    Don't clear ENS count in cumulative mode

--- src/senddata.cpp
+++ src/senddata.cpp
@@ -154,7 +154,8 @@ void sendData() {
       payload.addSensor(sensor_read(1));
       SendPayload(SENSOR1PORT, prio_normal);
 #if (COUNT_ENS)
-      cwa_clear();
+      if (cfg.countermode != 1)
+        cwa_clear();
 #endif
       break;
 #endif
