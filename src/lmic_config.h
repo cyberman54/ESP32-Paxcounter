a755a2d2701c09092d5a00e019dfbeb2c17c7817
Chrisotph Schultz <christoph@schultz1-chrisotph.de>
2020-10-12 21:24:48 +0200

    modified:   src/lmic_config.h

--- src/lmic_config.h
+++ src/lmic_config.h
@@ -36,7 +36,7 @@
 // so consuming more power. You may sharpen (reduce) this value if you are
 // limited on battery.
 // ATTN: VALUES > 7 WILL CAUSE RECEPTION AND JOIN PROBLEMS WITH HIGH SF RATES
-//#define CLOCK_ERROR_PROCENTAGE 5
+#define CLOCK_ERROR_PROCENTAGE 1
 
 // Set this to 1 to enable some basic debug output (using printf) about
 // RF settings used during transmission and reception. Set to 2 to
