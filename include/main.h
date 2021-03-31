36363ee3771c47a39185c14685fec5af8ce38474
cyberman54 <cyberman@obh.snafu.de>
2021-03-31 09:46:51 +0200

    main.h: added include coexist.h

--- include/main.h
+++ include/main.h
@@ -4,6 +4,7 @@
 #include <esp_spi_flash.h>   // needed for reading ESP32 chip attributes
 #include <esp_event_loop.h>  // needed for Wifi event handler
 #include <esp32-hal-timer.h> // needed for timers
+#include <esp_coexist.h>     // needed for coex version display
 
 #include "globals.h"
 #include "reset.h"
