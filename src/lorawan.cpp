76c465ccb7b05ad953b4c1a825294cc3d7bca3ae
cyberman54 <cyberman@obh.snafu.de>
2022-01-28 19:51:59 +0100

    change order of loraykey display for TTNv3 console

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -179,10 +179,10 @@ void showLoraKeys(void) {
   // all EUI buffer so we do it here to a temp
   // buffer to be able to display them
   uint8_t buf[32];
-  os_getDevEui((u1_t *)buf);
-  printKey("DevEUI", buf, 8, true);
   os_getArtEui((u1_t *)buf);
   printKey("AppEUI", buf, 8, true);
+  os_getDevEui((u1_t *)buf);
+  printKey("DevEUI", buf, 8, true);
   os_getDevKey((u1_t *)buf);
   printKey("AppKey", buf, 16, false);
 }
