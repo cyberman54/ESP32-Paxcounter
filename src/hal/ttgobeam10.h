2d05e1696e6393a12dffe65c250f3987d5405eeb
cyberman54 <cyberman@obh.snafu.de>
2022-09-17 18:34:30 +0200

    xpowers lib updates

--- src/hal/ttgobeam10.h
+++ src/hal/ttgobeam10.h
@@ -36,10 +36,10 @@ Reset -> reset device
 #define HAS_PMU 1 // has AXP192 chip
 #define XPOWERS_CHIP_AXP192 1
 #define PMU_INT GPIO_NUM_35 // battery interrupt
-#define PMU_CHG_CURRENT XPOWERS_CHG_CUR_1000MA // battery charge current
+#define PMU_CHG_CURRENT XPOWERS_AXP192_CHG_CUR_1000MA // battery charge current
 // possible values (mA):
 // 100/190/280/360/450/550/630/700/780/880/960/1000/1080/1160/1240/1320
-#define PMU_CHG_CUTOFF XPOWERS_CHG_VOL_4V2 // battery charge cutoff
+#define PMU_CHG_CUTOFF XPOWERS_AXP192_CHG_VOL_4V2 // battery charge cutoff
 // possible values (V):
 // 4V1/4V15/4V2/4V36
 
