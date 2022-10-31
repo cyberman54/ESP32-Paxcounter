b2c054f187653706951247e81cb41cb42f5983fc
cyberman54 <cyberman@obh.snafu.de>
2022-10-31 16:56:48 +0100

    RGB led library migration

--- src/hal/lolin32lora.h
+++ src/hal/lolin32lora.h
@@ -18,7 +18,8 @@
 #define HAS_LED NOT_A_PIN // Led os on same pin as Lora SS pin, to avoid problems, we don't use it
 #define LED_ACTIVE_LOW 1  // Onboard LED is active when pin is LOW
                           // Anyway shield is on over the LoLin32 board, so we won't be able to see this LED
-#define HAS_RGB_LED SmartLed rgb_led(LED_WS2812, 1, GPIO_NUM_13) // ESP32 GPIO13 (pin13) On Board Shield WS2812B RGB LED
+#define RGB_LED_COUNT 1 // we have 1 LED
+#define HAS_RGB_LED FastLED.addLeds<WS2812, GPIO_NUM_13, GRB>(leds, RGB_LED_COUNT);
 #define HAS_BUTTON    15  // ESP32 GPIO15 (pin15) Button is on the LoraNode32 shield
 #define BUTTON_PULLUP  1  // Button need pullup instead of default pulldown
 
--- src/hal/lopy.h
+++ src/hal/lopy.h
@@ -12,7 +12,8 @@
 #define HAS_LORA 1       // comment out if device shall not send data via LoRa
 #define CFG_sx1272_radio 1
 #define HAS_LED NOT_A_PIN // LoPy4 has no on board mono LED, we use on board RGB LED
-#define HAS_RGB_LED SmartLed rgb_led(LED_WS2812, 1, GPIO_NUM_0) // WS2812B RGB LED on GPIO0 (P2)
+#define RGB_LED_COUNT 1 // we have 1 LED
+#define HAS_RGB_LED FastLED.addLeds<WS2812, GPIO_NUM_0, GRB>(leds, RGB_LED_COUNT);
 
 // Note: Pins for LORA chip SPI interface come from board file pins_arduino.h
 
