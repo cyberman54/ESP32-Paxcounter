99eb2d3fb970e6dbff8362c4afb4b149bf4ec585
Verkehrsrot <cyberman@obh.snafu.de>
2026-08-04 23:07:58 +0200

    Comment out SD card configurations
    Comment out SD card related definitions in generic.h

--- shared/hal/generic.h
+++ shared/hal/generic.h
@@ -19,12 +19,12 @@
 //#define SPI_CS   GPIO_NUM_5
 
 // enable only if you want to store a local paxcount table on the device
-#define HAS_SDCARD  1      // this board has an SD-card-reader/writer
+//#define HAS_SDCARD  1      // this board has an SD-card-reader/writer
 // Pins for SD-card
-#define SDCARD_CS    (13)
-#define SDCARD_MOSI  (15)
-#define SDCARD_MISO  (2)
-#define SDCARD_SCLK  (14)
+//#define SDCARD_CS    (13)
+//#define SDCARD_MOSI  (15)
+//#define SDCARD_MISO  (2)
+//#define SDCARD_SCLK  (14)
 
 // enable only if device has these sensors, otherwise comment these lines
 // tutorial to connect BME sensor see here:
